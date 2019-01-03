cf334eff9296a73687a9a1630dcd2ce6a1138683
Amanda Lau <amalau1996@gmail.com>
2019-01-02 21:37:12 -0500

    Graph labels and colors

--- Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
+++ Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.dfrobot.angelo.blunobasicdemo;
 
 // From original app(
-import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Environment;
@@ -25,76 +24,113 @@ import java.text.SimpleDateFormat;
 //For keeping the app awake(
 import android.view.WindowManager;
 //)
-
+//For visualizing the graph(
+import java.util.Iterator;
+import android.graphics.Color;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-
+import com.jjoe64.graphview.LegendRenderer;
+//)
 
 public class MainActivity  extends BlunoLibrary {
 	private Button buttonScan;
 	private Button buttonSerialSend;
 	private ToggleButton buttonScreen;
 	private TextView serialReceivedText;
-
 	private SimpleDateFormat dateFormat;
 	private String storagePath;
-	private LineGraphSeries series;
-    private double lastXValue = 0d;
-    private static final int NOTIFICATION_ID = 1;
+	private GraphView graph;
+	private static int pointMax = 60;
+	private static int minuteMax = 480;
+	private LineGraphSeries secondSeriesA;
+    private LineGraphSeries secondSeriesB;
+	private LineGraphSeries minuteSeries;
+    private double secCounter = 0d;
+    private double minCounter = 0d;
+    private int state = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-        onCreateProcess();														//onCreate Process by BlunoLibrary
+        //onCreate Process by BlunoLibrary
+        onCreateProcess();
         // Set default date format for text log
         dateFormat = new SimpleDateFormat("yyyy/MM/dd,HH:mm:ss");
         // Set default directory for text log
         storagePath = Environment.getExternalStorageDirectory().getPath();
-        // Create a new series object of DataPoints
-        series = new LineGraphSeries<DataPoint>();
-
-        serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
-
-        serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);	//initial the EditText of the received data
-        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
+        // Create a new secondSeries object of DataPoints
+        secondSeriesA = new LineGraphSeries<DataPoint>();
+        secondSeriesB = new LineGraphSeries<DataPoint>();
+		minuteSeries = new LineGraphSeries<DataPoint>();
+        //set the Uart Baudrate on BLE chip to 115200
+        serialBegin(115200);
+        //initial the EditText of the received data
+        serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);
+        //initial the button for sending the data
+        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);
         buttonSerialSend.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				serialSend("");				//send the data to the BLUNO
+                //send the data to the BLUNO
+				serialSend("");
 			}
 		});
-
-        buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
+        //initial the button for scanning the BLE device
+        buttonScan = (Button) findViewById(R.id.buttonScan);
         buttonScan.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
+                //Alert Dialog for selecting the BLE device
+				buttonScanOnClickProcess();
 			}
 		});
 
-        buttonScreen = (ToggleButton) findViewById(R.id.buttonScreenOn);					//initial the button for scanning the BLE device
+        //initial the button for keeping the device from falling asleep
+        buttonScreen = (ToggleButton) findViewById(R.id.buttonScreenOn);
 		buttonScreen.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				buttonScreenOnCheckedProcess(isChecked);
 			}
 		});
-		GraphView graph = findViewById(R.id.serialGraphView);
-        graph.getViewport().setScalable(true);
-		graph.addSeries(series);
+		// Set up graph
+        initialize_graph();
+
 	}
+
+	private void initialize_graph(){
+        graph = findViewById(R.id.serialGraphView);
+        // Set axis bounds
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(2 * pointMax);
+        //Set titles for legend
+        secondSeriesA.setTitle(getString(R.string.seconds_title));
+        secondSeriesB.setTitle(getString(R.string.seconds_title));
+        minuteSeries.setTitle(getString(R.string.minutes_title));
+        //Set colors for graphing. Second series A and B should be the same color
+        secondSeriesA.setColor(Color.rgb(255,178,102));
+        secondSeriesB.setColor(Color.rgb(255,178,102));
+        minuteSeries.setColor(Color.rgb(115,255,255));
+        //Show and align legend
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graph.getLegendRenderer().setVisible(true);
+        //Set titles
+        graph.getGridLabelRenderer().setHorizontalAxisTitle(getString(R.string.hor_title));
+        graph.getGridLabelRenderer().setVerticalAxisTitle(getString(R.string.vert_title));
+        //Add series to graph
+        graph.addSeries(secondSeriesA);
+        graph.addSeries(minuteSeries);
+        graph.getGridLabelRenderer().setPadding(32);
+    }
 
 	protected void onResume(){
 		super.onResume();
 		System.out.println("BlUNOActivity onResume");
 		onResumeProcess();														//onResume Process by BlunoLibrary
 	}
-	
-	
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
@@ -104,23 +140,28 @@ public class MainActivity  extends BlunoLibrary {
     @Override
     protected void onPause() {
         super.onPause();
-        onPauseProcess();														//onPause Process by BlunoLibrary
+        //onPause Process by BlunoLibrary
+        onPauseProcess();
     }
-	
+
 	protected void onStop() {
 		super.onStop();
-		onStopProcess();														//onStop Process by BlunoLibrary
+        //onStop Process by BlunoLibrary
+		onStopProcess();
 	}
     
 	@Override
     protected void onDestroy() {
-        super.onDestroy();	
-        onDestroyProcess();														//onDestroy Process by BlunoLibrary
+        super.onDestroy();
+        //onDestroy Process by BlunoLibrary
+        onDestroyProcess();
     }
 
 	@Override
-	public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
-		switch (theConnectionState) {											//Four connection state
+    //Once connection state changes, this function will be called
+	public void onConectionStateChange(connectionStateEnum theConnectionState) {
+		switch (theConnectionState) {
+        //Four connection states
 		case isConnected:
 			buttonScan.setText("Connected");
 			break;
@@ -143,10 +184,10 @@ public class MainActivity  extends BlunoLibrary {
 
 	public void buttonScreenOnCheckedProcess(boolean isChecked){
 		if (isChecked) {
-			// The toggle is enabled
+			// The toggle is enabled; keep the screen on until button is unchecked
 			getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		} else {
-			// The toggle is disabled
+			// The toggle is disabled; allow screen to fall asleep
 			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		}
 	}
@@ -154,11 +195,9 @@ public class MainActivity  extends BlunoLibrary {
 	@Override
 	public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
 		// TODO Auto-generated method stub
-        // Add data point to graph
+        // Get data point value
         double newYValue= Double.parseDouble(theString);
-        series.appendData(new DataPoint(lastXValue, newYValue), false, 300);
         ((ScrollView)serialReceivedText.getParent()).fullScroll(View.FOCUS_DOWN);
-        lastXValue += 1d;
         // Format datapoint with a timestamp
         theString = getTimestamp() + "," + theString;
         //append the text into the EditText
@@ -166,7 +205,69 @@ public class MainActivity  extends BlunoLibrary {
 		// Append to text file
         appendLog(theString);
 		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
+		//process data point
+        processNewValue(newYValue);
 	}
+
+	private void processNewValue(double value){
+		secCounter += 1d;
+		switch (state) {
+            case 0:
+                secondSeriesA.appendData(new DataPoint(secCounter,value),false,2*pointMax,false);
+                if((int)secCounter == pointMax){
+                    minCounter += 1d;
+                    minuteSeries.appendData(new DataPoint(minCounter,averageSeries(secondSeriesA)),false,minuteMax,false);
+                    secCounter = 0;
+                    state = 2;
+                }
+            case 1:
+                secondSeriesA.appendData(new DataPoint(secCounter,value),false,2*pointMax,false);
+                secondSeriesB.appendData(new DataPoint(secCounter +60,value),false,2*pointMax,false);
+                if ((int)secCounter == pointMax){
+                    minCounter += 1d;
+                    minuteSeries.appendData(new DataPoint(minCounter,averageSeries(secondSeriesA)),false,minuteMax,false);
+                    secCounter = 0;
+                    state = 3;
+                }
+            case 2:
+                secondSeriesB.appendData(new DataPoint(secCounter,value),false,2*pointMax,false);
+                secondSeriesA.appendData(new DataPoint(secCounter +60,value),false,2*pointMax,false);
+                if ((int)secCounter == pointMax){
+                    minCounter += 1d;
+                    minuteSeries.appendData(new DataPoint(minCounter,averageSeries(secondSeriesB)),false,minuteMax,false);
+                    secCounter = 0;
+                    state = 4;
+                }
+            case 3:
+                secondSeriesA.appendData(new DataPoint(secCounter +60,value),false,2*pointMax,false);
+                graph.removeSeries(secondSeriesB);
+                graph.addSeries(secondSeriesA);
+                DataPoint[] newSeriesB = new DataPoint[1];
+                newSeriesB[0] = new DataPoint(secCounter,value);
+                secondSeriesB.resetData(newSeriesB);
+                state = 2;
+            case 4:
+                secondSeriesB.appendData(new DataPoint(secCounter +60,value),false,2*pointMax,false);
+                graph.removeSeries(secondSeriesA);
+                graph.addSeries(secondSeriesB);
+                DataPoint[] newSeriesA = new DataPoint[1];
+                newSeriesA[0] = new DataPoint(secCounter,value);
+                secondSeriesB.resetData(newSeriesA);
+                state = 1;
+        }
+	}
+
+	private double averageSeries(LineGraphSeries E){
+	    Iterator values = E.getValues(E.getLowestValueX(),E.getHighestValueX());
+	    double count = 0;
+	    double sum = 0;
+	    while(values.hasNext()){
+	        sum += (double)values.next();
+	        values.remove();
+	        count += 1;
+        }
+        return sum/(double)count;
+    }
 
 	public String getTimestamp(){
 	    long time = System.currentTimeMillis();
